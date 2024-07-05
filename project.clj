6f754de2e30c7919819131fff4662146daa8fb2e
licht1stein <mb@m1k.pw>
2024-07-05 14:24:08 +0100

    Switched from project.clj to deps.edn and removed compojure

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject ring-refresh "0.2.0"
-  :description "Ring middleware for automatically refreshing HTML pages"
-  :url "https://github.com/weavejester/ring-refresh"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [watchtower "0.1.1"]
-                 [compojure "1.7.1"]])
